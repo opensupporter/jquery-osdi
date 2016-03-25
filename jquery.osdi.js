32f683d91f40814669edfe2d73de66417410d971
Jason Rosenbaum <seminal@theseminal.com>
2016-03-25 07:38:27 -0700

    bump version, add license

--- jquery.osdi.js
+++ jquery.osdi.js
@@ -4,29 +4,18 @@
 	
 	Submit forms to OSDI-compatible systems using OSDI's non-authenticated POST functions and triggers. Requires jQuery 1.8 or above. More info on OSDI at http://opensupporter.org/
 	
-	Version: 0.1.2
+	Version: 1.0.0
 	Last Updated: March 8, 2016
 	Authors: Jason Rosenbaum
-	Repository & License: 
+	Repository: https://github.com/opensupporter/jquery-osdi
+	License: MIT open source, https://github.com/opensupporter/jquery-osdi/blob/master/LICENSE.md
 	
 ************************************************************/
 
-// the semi-colon before function invocation is a safety net against concatenated
-// scripts and/or other plugins which may not be closed properly.
 ;( function( $, window, document, undefined ) {
 
 	"use strict";
-
-		// undefined is used here as the undefined global variable in ECMAScript 3 is
-		// mutable (ie. it can be changed by someone else). undefined isn't really being
-		// passed in so we can ensure the value of it is truly undefined. In ES5, undefined
-		// can no longer be modified.
-
-		// window and document are passed through as local variable rather than global
-		// as this (slightly) quickens the resolution process and can be more efficiently
-		// minified (especially when both are regularly referenced in your plugin).
-
-		// Create the defaults once
+	
 		var pluginName = "osdi",
 			defaults = {
 				autoresponse: true,
@@ -50,11 +39,6 @@
 		function Plugin ( element, options ) {
 			//console.log($element.length);
 			this.$element = $(element);
-
-			// jQuery has an extend method which merges the contents of two or
-			// more objects, storing the result in the first object. The first object
-			// is generally empty as we don't want to alter the default options for
-			// future instances of the plugin
 			
 			this.settings = $.extend( {}, defaults, options );
 			this._defaults = defaults;
@@ -71,13 +55,6 @@
 		// Avoid Plugin.prototype conflicts
 		$.extend( Plugin.prototype, {
 			init: function($element) {
-
-				// Place initialization logic here
-				// You already have access to the DOM element and
-				// the options via the instance, e.g. this.element
-				// and this.settings
-				// you can add more functions like the one below and
-				// call them like the example bellow
 				
 				// validate to make sure we can use this form on a basic level
 				if (this.validate_form( $element )) {
